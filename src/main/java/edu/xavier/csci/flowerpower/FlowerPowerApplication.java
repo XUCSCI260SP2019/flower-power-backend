685b184c81bd06b97eeccfe82a770a3e77e07345
Daniel McMullen <dmcmullen@gmail.com>
2019-04-02 18:27:19 -0400

    Daniel McMullen | Barebones spring app structure

---
+++ src/main/java/edu/xavier/csci/flowerpower/FlowerPowerApplication.java
@@ -0,0 +1,13 @@
+package edu.xavier.csci.flowerpower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowerPowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowerPowerApplication.class, args);
+	}
+
+}
