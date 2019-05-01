a9302a329650333bc951b6d7c37dec1f5670a14d
AndrewBraun3 <AndrewBraun3@users.noreply.github.com>
2019-05-01 17:55:26 -0400

    added more hardcoded students and professors

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,19 @@
 INSERT INTO professor (email, password, full_name) VALUES
-('email1@email.com', 'password', 'joe bob'),
-('email2@email.com', 'password2', 'bob joe');
+('pEmail_@email.com', 'pPassword', 'bob tim'),
+('pEmail_2@email.com.com', 'pPassword2', 'tim bob'),
+('pEmail_3@email.com', 'pPassword3', 'not tim');
 
 INSERT INTO student (email, password, full_name) VALUES
-('email1@email.com', 'password', 'jim bob'),
-('email2@email.com', 'password2', 'bob jim');
+('sEmail_@email.com', 'password', 'jim bob'),
+('sEmail_2@email.com', 'password2', 'bob jim'),
+('sEmail_3@email.com', 'sPassword3', 'not bob');
+
+
+INSERT INTO professor_student_link (professor_id, student_id) VALUES
+(1, 1),
+(1, 2),
+(1, 3),
+(2, 2),
+(2, 3),
+(3, 3),
+(3, 1);
