eb53b6d36de126860a15ac00ad348e408e9cf54e
Daniel McMullen <dmcmullen@gmail.com>
2019-05-01 16:29:36 -0400

    daniel mcmullen | create dummy data but broke routings

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO professors (email, password, full_name) VALUES
+('email1@email.com', 'password', 'joe bob'),
+('email2@email.com', 'password2', 'bob joe');
+
+INSERT INTO students (email, password, full_name) VALUES
+('email1@email.com', 'password', 'jim bob'),
+('email2@email.com', 'password2', 'bob jim');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -13,6 +13,21 @@ create table if not exists reservation(
     cancelled bool,
     cancellation_reason text
 );
+
+create table if not exists professors(
+    id int auto_increment primary key,
+    email varchar(255),
+    password varchar(255),
+    full_name varchar(255)
+);
+
+create table if not exists students(
+    id int auto_increment primary key,
+    email varchar(255),
+    password varchar(255),
+    full_name varchar(255)
+);
+
 create table if not exists professor_student_link(
     professor_id int,
     student_id int
