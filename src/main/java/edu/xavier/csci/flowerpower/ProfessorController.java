a1287b10b45e0cd962b97fd953f89cdef989d7a0
AlexDeLaet <delaetalex.6@gmail.com>
2019-05-01 14:25:34 -0400

    added the spring framework to the other controllers

--- src/main/java/edu/xavier/csci/flowerpower/ProfessorController.java
+++ src/main/java/edu/xavier/csci/flowerpower/ProfessorController.java
@@ -5,6 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProfessorController {
