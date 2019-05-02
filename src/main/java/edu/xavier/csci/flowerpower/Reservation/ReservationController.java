4aad3088c29f1fc7153d1a928bf46602093aa587
AlexDeLaet <delaetalex.6@gmail.com>
2019-05-02 11:03:25 -0400

    Added cross origin to student and reservation

--- src/main/java/edu/xavier/csci/flowerpower/Reservation/ReservationController.java
+++ src/main/java/edu/xavier/csci/flowerpower/Reservation/ReservationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "*")
 public class ReservationController {
     private static final Logger logger = LoggerFactory.getLogger(ReservationController.class);
 
